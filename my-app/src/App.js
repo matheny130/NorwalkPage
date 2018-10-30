f4dc2f75aedb3f843598aaa381707741cf63be0a
Mike Matheny <mike.matheny.81@gmail.com>
2018-10-29 20:29:40 -0400

    added pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,9 @@ import { Route, Router } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Callback from "./Callback/Callback";
 import Navbar from "./components/Navbar/Navbar";
-import Park from "./pages/Park";
-import School from "./pages/School";
-import Business from "./pages/Business"
+import Park from "./components/Pages/Park";
+import School from "./components/Pages/School";
+import Business from "./components/Pages/Business"
 import './App.css';
 
 const App = () => (
@@ -15,9 +15,9 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route exact path="/park" component={Park} />
       <Route exact path="/school" component={School} />
-      <Route exact path ="/business" component={Business} />
+      <Route exact path="/business" component={Business} />
     </div>
   </Router>
-)
+);
 
 export default App;
