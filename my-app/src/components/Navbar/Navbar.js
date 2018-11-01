1b0f6ecd7ab610e8d03b81877c7b33447099614b
Mike Matheny <mike.matheny.81@gmail.com>
2018-11-01 16:59:21 -0400

    working on navbar

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import history from "../../history";
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import './Navbar.css'
 
 class NavExample extends React.Component {
@@ -41,7 +41,7 @@ class NavExample extends React.Component {
           <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
-
+              <UncontrolledDropdown>
                 <DropdownToggle nav caret>
                   Parks
                 </DropdownToggle>
@@ -87,6 +87,7 @@ class NavExample extends React.Component {
                     Reset
                   </DropdownItem>
                 </DropdownMenu>
+              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
