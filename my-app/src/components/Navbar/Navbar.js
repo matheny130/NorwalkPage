e915ef87d6c7100b53d7fbc531c6df7b0298c0c3
Mike Matheny <mike.matheny.81@gmail.com>
2018-11-03 11:53:57 -0400

    working on navbar

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -1,34 +1,11 @@
 import React from "react";
 //import history from "../../history";
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, Nav, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavLink, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import './Navbar.css'
 
 class NavExample extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
 
-  toggleNavbar() {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-  goTo(route) {
-    this.props.history.replace(`/${route}`)
-  }
-
-  login() {
-    this.props.auth.login();
-  }
-
-  logout() {
-    this.props.auth.logout();
-  }
 
 
   render() {
@@ -36,60 +13,17 @@ class NavExample extends React.Component {
 
       <div id="nav">
         <Navbar color="faded" light>
-          <NavbarBrand href="/" className="text-center"><p id="logo">City of Norwalk</p></NavbarBrand>
+          <NavbarBrand href="/" className="text-center"><p>City of Norwalk</p></NavbarBrand>
 
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse isOpen={!this.state.collapsed} navbar>
+
             <Nav navbar>
-              <UncontrolledDropdown>
-                <DropdownToggle nav caret>
-                  Parks
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-                <DropdownToggle nav caret>
-                  Schools
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-                <DropdownToggle nav caret>
-                  Businesses
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
+              <NavLink>
+                Parks
+
+               </NavLink>
+
             </Nav>
-          </Collapse>
+
         </Navbar>
       </div>
     );
