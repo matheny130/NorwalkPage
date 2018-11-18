8e1adac4dbcc009b0ac69467ce00765017d6c705
Mike Matheny <mike.matheny.81@gmail.com>
2018-11-17 20:46:28 -0500

    working on secondary pages

--- my-app/src/components/Pages/Community/communityHome.js
+++ my-app/src/components/Pages/Community/communityHome.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import "./schools.css";
-import School from "./schoolsHome.js";
+import "./community.css";
+
 
 
 
@@ -15,18 +15,24 @@ class Community extends React.Component {
   render() {
     return (
       <Router>
-        <div id="page">
-          <ul>
-            <li>
-              <Link to="/school">Schools</Link>
-            </li>
+        <div class="page">
+          <div class="row">
+            <div class="col-lg-4-xs-12 phone-nav" id="links">
+              <div class="col-lg-1-xs-4 phone-nav" id="community-link">
+                <a id="navbar-link" href="/">Home</a>
+              </div>
+              <div class="col-lg-1-xs-4 phone-nav" id="community-link">
+                <a id="navbar-link" href="/schoolsHome">Schools</a>
+              </div>
+              <div class="col-lg-1-xs-4 phone-nav" id="community-link">
+                <a id="navbar-link" href="/schools/league">Hospital</a>
+              </div>
+              <div class="col-lg-1-xs-4 phone-nav" id="community-link">
+                <a id="navbar-link" href="/schools/highSchool">History</a>
+              </div>
 
-          </ul>
-
-          <hr />
-
-          <Route exact path="/school" component={School} />
-
+            </div>
+          </div>
         </div>
       </Router>
     );
